33e067f4bc898c9de48da91238054f18f457ba0b
robinsonha <126197658+robinsonha@users.noreply.github.com>
2023-09-16 00:05:58 +0100

    Update build c_pitching model new.R
    Added correlation code options, removed highly correlated variables, write feature importance to file for comparisons

--- build c_pitching model new.R
+++ build c_pitching model new.R
@@ -3,20 +3,23 @@ library(tidyverse)
 options(scipen=999)
 library(xgboost)
 library(modelr)
-
+library(caret)
+library(tidymodels)
+library(mlr)
+library(Hmisc)
 
 # read in data
 s2017 <- read_csv('statcast_2017.csv')
 s2018 <- read_csv('statcast_2018.csv')
 s2019 <- read_csv('statcast_2019.csv') %>% filter(game_date > "2019-03-27")
-s2020 <- read_csv('statcast_2020.csv') 
+#s2020 <- read_csv('statcast_2020.csv') #currently unavailable
 s2021 <- read_csv('statcast_2021.csv')
 s2022 <- read_csv('statcast_2022.csv') %>% filter(game_date > "2022-04-06")
 s2023 <- read_csv('statcast_2023.csv')
 
 ## Bring data together. Four total Pitching models are built, one for each p_throws / stand combination. For each one, filters must be changed below 
-mlbraw <- bind_rows(s2017, s2018, s2019, s2021, s2022, s2023, s2020) %>% distinct() %>% filter(p_throws == 'L' & stand == 'R' & game_type == "R")
-rm(s2022, s2021, s2019, s2018, s2017, s2023, s2020)
+mlbraw <- bind_rows(s2017, s2018, s2019, s2021, s2022, s2023) %>% distinct() %>% filter(p_throws == 'L' & stand == 'R' & game_type == "R")
+rm(s2022, s2021, s2019, s2018, s2017, s2023)
 gc()
 
 ### Filter out pitchers hitting, besides Ohtani. This filters out any hitter that's thrown 75 or more pitches between 2017-2022
@@ -77,10 +80,21 @@ pitcher_fastballs <- mlbraw1 %>% filter(pitch_type %in% c("FF", "FC", "SI")) %>%
 mlbraw2 <- mlbraw1 %>% left_join(pitcher_fastballs, by = c("year", "pitcher")) %>% mutate(spin_dif = spin_axis - fb_axis, velo_dif = release_speed-fb_velo,
                      ivb_dif = fb_max_ivb-pfx_z, break_dif = (fb_max_x*.5+fb_min_x*.5)-pfx_x)
 
+##"set" is a marker used further down to manually control variable subsetting (can be omitted if this is to be automated)
+if(mlbraw2$p_throws[1] == 'L' & mlbraw2$stand[1] == 'R' & mlbraw2$game_type[1] == "R"){
+  mlbraw2$set<-1
+} else if(mlbraw2$p_throws[1] == 'L' & mlbraw2$stand[1] == 'L' & mlbraw2$game_type[1] == "R"){
+  mlbraw2$set<-2
+} else if(mlbraw2$p_throws[1] == 'R' & mlbraw2$stand[1] == 'L' & mlbraw2$game_type[1] == "R"){
+  mlbraw2$set<-3
+} else {
+  mlbraw2$set<-4
+}
+
 ## filter for only variables to be used in model
 final_vars <- mlbraw2 %>% select(dre_final, starts_with("fb_"), release_speed, release_spin_rate, count, above_zone, below_zone,
                                  release_extension, release_pos_x, release_pos_z, above_zone, below_zone, pfx_x, pfx_z, zone_x_max, zone_x_min,
-                                 plate_x, plate_z, pitch_type, spin_axis, spin_dif, velo_dif, ivb_dif, break_dif) 
+                                 plate_x, plate_z, pitch_type, spin_axis, spin_dif, velo_dif, ivb_dif, break_dif, set) 
   
 
 ## create dummy variables where needed
@@ -96,8 +110,33 @@ rm(list = setdiff(ls(), "vars"))
 gc()
 
 set.seed(2552)
-library(tidymodels)
-library(mlr)
+##Test for correlations between variables
+
+corr_vars <- rcorr(as.matrix(vars))
+flattenCorrMatrix <- function(cormat, pmat) {
+  ut <- upper.tri(cormat)
+  data.frame(
+    row = rownames(cormat)[row(cormat)[ut]],
+    column = rownames(cormat)[col(cormat)[ut]],
+    cor  =(cormat)[ut],
+    p = pmat[ut]
+  )
+}
+
+mat<-flattenCorrMatrix(corr_vars$r, corr_vars$P)
+mat<-mat[mat$cor>0.95 | mat$cor< -0.95,]
+view(mat)
+
+##Create an alternative data table with one of each pair of highly correlated (>0.95) variables removed
+##To supervise and complete this manually:
+
+vars<-vars %>%
+  select(-c('fb_velo','below_zone','zone_x_min','plate_x','plate_z','spin_dif'))
+
+##OR to automate this process:
+#mat<-data.frame(mat)
+#vars<-vars[,!names(vars) %in% mat$column]
+
 
 #split into training (70%) and testing set (30%)
 c_pitching_split <- initial_split(vars, strata = dre_final, prop = .75) 
@@ -153,9 +192,11 @@ lrn_tune <- setHyperPars(lrn,par.vals = mytune$x)
 #train model
 xgmodel <- train(learner = lrn_tune,task = traintask)
 
-## view feature importance
+## view feature importance and record as csv for each model
 imp <- mlr::getFeatureImportance(xgmodel)
 view(imp$res)
+write.csv(imp$res,file=paste0("feature_importance_xgmodel",set,".csv"))
+
 
 #predict on test data
 xgpred <- predict(xgmodel,testtask) %>% as.data.frame()
